8d6cf3c5007a2155f2639919c89c21667859c91f
Thunder Wu <softengineerwu@gmail.com>
2016-07-28 21:04:35 +0800

    create new unit test.

---
+++ Winxuan.Service.Test/TestUserTeamService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.Data;
+using Winxuan.Infrastructure;
+using Winxuan.Infrastructure.DTO;
+using Winxuan.Service.Impl;
+using Winxuan.Service.Interfaces;
+
+namespace Winxuan.Service.Test
+{
+    [TestClass]
+    public class TestUserTeamService:TestBase
+    {
+        private IUserTeamService service;
+
+        public TestUserTeamService()
+        {
+            this.service = new UserTeamService(MockContext.Object);
+        }
+
+        /// <summary>
+        /// GetTeams. Success.
+        /// </summary>
+        [TestMethod]
+        public void TestGetTeams_Success()
+        {
+            var result = service.GetTeams(2);
+            var response = WebUtils.DeserializeObject<IList<UserTeamDTO>>(result.Result);
+            Assert.IsTrue(response.Status);
+            Assert.AreEqual(response.Data.Count, 2);
+        }
+
+        /// <summary>
+        /// GetTeams. Fail. Error user id. 
+        /// </summary>
+        [TestMethod]
+        public void TestGetTeams_Fail_ErrorUserId()
+        {
+            var result = service.GetTeams(-1);
+            var response = WebUtils.DeserializeObject<IList<UserTeamDTO>>(result.Result);
+            Assert.IsTrue(response.Status);
+            Assert.AreEqual(response.Data.Count, 0);
+        }
+    }
+}
